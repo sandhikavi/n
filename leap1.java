17117efb5e76421843975835af8f47183133a53c
sandhikavi <sandhyav.velusamy@gmail.com>
2016-08-16 18:43:48 +0530

    Update and rename leap.java to leap1.java

---
+++ leap1.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+class leap1{
+	
+public static void main (String[] args)
+{
+int i;	
+if(i%4==0)&&(i%100!=0)||(i%400==0)
+{
+System.out.println("the number is leapyear");
+}
+else
+
+{
+System.out.println("the number is not a leapyear");
+}	
+}
+
+}
